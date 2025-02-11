a918202a7b23d92a3c5eba320ea4e5e16cf7763a
Baruti-1 <barutibaruani1@mail.com>
2025-02-11 21:12:47 +0300

    Create product model

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { connectDb } from "./config/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -11,7 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/products", productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
