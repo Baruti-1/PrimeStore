ad55a576cf002aa8ce0ac77839026f9176bf88a1
Baruti-1 <barutibaruani1@mail.com>
2025-02-12 22:09:02 +0300

    Create middlewares for authorization

---
+++ api/middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - no access token provided" });
+    }
+
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Invalid access token", error: error.message });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "No access - Admin only" });
+  }
+};
