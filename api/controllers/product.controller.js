ef41458a935e7c1b5c2d811e8580bb51387a8ca0
Baruti-1 <barutibaruani1@mail.com>
2025-02-12 22:27:48 +0300

    Implement an endpoint to get featured products

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,9 +1,29 @@
 import Product from "../models/product.model.js";
+import { redis } from "../config/redis.js";
 
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) return res.json(JSON.parse(featuredProducts));
+
+    // if featured products are not is redis
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts)
+      return res.status(404).json({ message: "No featured products found" });
+
+    // store in redis for quick access
+    await redis.set("featured_products", JSON.stringify(featuredProducts));
+    res.status(200).json(featuredProducts);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
