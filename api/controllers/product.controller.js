cb52e70f0af41331695a079e16715c899fb3173b
Baruti-1 <barutibaruani1@mail.com>
2025-02-13 07:17:29 +0300

    Implement endpoint to delete product

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -57,3 +57,26 @@ export const getFeaturedProducts = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.product_id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    if (product.image) {
+      const publicId = product.image.split("/").pop().split(".")[0]; // image id from cloudinary
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+      } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+      }
+
+      await Product.findByIdAndDelete(req.params.product_id);
+      res.json({ message: "Product deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -3,6 +3,7 @@ import {
   createProduct,
   getAllProducts,
   getFeaturedProducts,
+  deleteProduct,
 } from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middlewares/auth.middleware.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", protectRoute, adminRoute, createProduct);
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.delete("/:product_id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
