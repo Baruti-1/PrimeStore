4d07c2684809a4a93a816c2cf5533df9d534a84f
Baruti-1 <barutibaruani1@mail.com>
2025-02-19 11:38:17 +0300

    Implement user profile api endpoint

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,17 +3,17 @@ import {
   signin,
   signout,
   signup,
+  getProfile,
   refreshToken,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-
 router.post("/signin", signin);
-
 router.post("/signout", signout);
-
 router.post("/refresh-token", refreshToken);
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
