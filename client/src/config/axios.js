bcc7152dff3b50694456d035bbab11484182d7b4
Baruti-1 <barutibaruani1@mail.com>
2025-02-20 22:04:26 +0300

    Create axios instance

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = new axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL:
+    import.meta.mode === "development"
+      ? "http://localhost:3000/api/v1"
+      : "/api/v1",
   withCredentials: true, // send cookies to the server
 });
 
