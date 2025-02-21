61c36d7bf58da9ea8ffa59e90475f19d70be8a5d
Baruti-1 <barutibaruani1@gmail.com>
2025-02-21 15:29:44 +0300

    Create Admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import AdminPage from "./pages/AdminPage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import LoadingSpinner from "./components/LoadingSpinner.jsx";
 import { Toaster } from "react-hot-toast";
@@ -36,6 +37,16 @@ function App() {
           <Route
             path="/login"
             element={!user ? <LoginPage /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path="/secret-dashboard"
+            element={
+              user?.role === "admin" ? (
+                <AdminPage />
+              ) : (
+                <Navigate to={"/login"} />
+              )
+            }
           />
         </Routes>
       </div>
